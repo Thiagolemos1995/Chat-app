29f62d1837d92f52fd430d78b8df59f27a11818d
Thiago Lemos Silva <thiagolemos1995@hotmail.com>
2021-06-19 11:38:56 -0300

    Update Server-side
    Foi adicionado mais algumas funções no server-side.
    Verificação se o server está rodando.
    Verificação se teve alguma conexão/desconexão com o client-side

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
+//Import
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
+//Define a porta onde irá rodar o servidor
 const PORT = process.env.PORT || 5000
+
+//Import do router.js
+const router = require('./router')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+//Irá mostrar uma mensagem no console caso algum usuário tenha se conectado ou desconectado
+io.on('connection', (socket) => {
+    console.log('Um usuário se conectou')
+
+    socket.on('disconnect', ()=>{
+        console.log('Um usuário se desconectou')
+    })
+})
+
+app.use(router)
 
 server.listen(PORT, () => console.log(`Server has started on port: ${PORT}`))
---
+++ server/router.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+//Quando o servidor estiver rodando irá mostrar esta mensagem
+router.get('/', (req, res)=> {
+    res.send('O servidor está rodando')
+})
+
+module.exports = router
